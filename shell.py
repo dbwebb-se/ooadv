9c977ebddd54da82040b498e9588e5e0ab08228c
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-01-24 13:32:04 +0100

    Shell: Add ctrl-d as alternative to exit.

--- shell.py
+++ shell.py
@@ -14,7 +14,10 @@ class Shell(cmd.Cmd):
     intro = "Welcome to the XXX shell. Type help or ? to list commands.\n"
     prompt = "(XXX) "
 
-    def do_hi(self):
+    # Do not show in help
+    __hidden_methods = ('do_EOF',)
+
+    def do_hi(self, _):
         """
         Say hi.
         """
@@ -26,12 +29,27 @@ class Shell(cmd.Cmd):
         """
         print("Hi there {}. How are you today?".format(arg))
 
-    def do_exit(self):
+    def emptyline(self):
+        """
+        Ignore when line is empty.
+        """
+        pass
+
+    def get_names(self):
+        l1 = dir(self.__class__)
+        l2 = self.__hidden_methods
+        return [x for x in l1 if x not in l2]
+
+    def do_exit(self, _):
         """
         Leave.
         """
         print("Bye bye - see ya soon again")
         return True
 
-    def emptyline(self):
-        pass
+    def do_EOF(self, _):
+        """
+        Exit when ctrl-d
+        """
+        print()
+        return self.do_exit(_)
