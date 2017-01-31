63e8c73ac4ae5b12b4a111f84f521f2fbb470ba1
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-02-01 00:04:34 +0100

    Move all tests to own directory

---
+++ tests/test_usage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+"""
+Unittest for the usage module.
+"""
+
+import unittest
+from usage import Usage
+
+
+class TestUsageModule(unittest.TestCase):
+    """Test cases"""
+
+    def setUp(self):
+        """Setup each testcase"""
+        self.usage = Usage()
+
+    def test_silent_verbose(self):
+        """
+        --silent
+        --verbose
+        --silent --verbose
+        """
+        options = self.usage.parseOptions("".split())
+        self.assertFalse(options["silent"])
+        self.assertFalse(options["verbose"])
+
+        self.usage.__init__()
+        options = self.usage.parseOptions("--silent".split())
+        self.assertTrue(options["silent"])
+        self.assertFalse(options["verbose"])
+
+        self.usage.__init__()
+        options = self.usage.parseOptions("--verbose".split())
+        self.assertFalse(options["silent"])
+        self.assertTrue(options["verbose"])
+
+        self.usage.__init__()
+        options = self.usage.parseOptions("--verbose --silent".split())
+        self.assertTrue(options["silent"])
+        self.assertTrue(options["verbose"])
+
+    def test_version(self):
+        """
+        -v
+        --version
+        """
+        with self.assertRaises(SystemExit):
+            self.usage.parseOptions("-v".split())
+
+        with self.assertRaises(SystemExit):
+            self.usage.parseOptions("--version".split())
+
+    def test_help(self):
+        """
+        -h
+        --help
+        """
+        with self.assertRaises(SystemExit):
+            self.usage.parseOptions("-h".split())
+
+        with self.assertRaises(SystemExit):
+            self.usage.parseOptions("--help".split())
+
+
+if __name__ == "__main__":
+    unittest.main()
